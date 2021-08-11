dfd4380d1dce03d23cbe76c069c585ff0bd07ab8
Nick Schneble <nschneble@users.noreply.github.com>
2021-08-11 15:33:27 -0400

    let's re-add some validations

--- lib/grape/attack/options.rb
+++ lib/grape/attack/options.rb
@@ -3,7 +3,13 @@ require "active_model"
 module Grape
   module Attack
     class Options
+      include ActiveModel::Model
+      include ActiveModel::Validations
+
       attr_accessor :limit, :per, :identifier, :remaining
+
+      validates :limit, numericality: { allow_nil: true, only_integer: true, greater_than: 0 }
+      validates :per, numericality: { allow_nil: true, only_integer: true, greater_than: 0 }
 
       def limit
         @limit || ::Grape::Attack.config.throttle_limit
@@ -18,7 +24,7 @@ module Grape
       end
 
       def throttle_endpoint_individually?
-        @limit && @per
+        @limit.present? && @per.present?
       end
     end
   end
